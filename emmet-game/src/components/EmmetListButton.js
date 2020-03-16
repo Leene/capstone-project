e04ba87646d6a502e82ea7a22791eebaa713050f
marleenstuber <marleenstuber@marleens-mbp.localdomain>
2020-03-16 12:43:00 +0100

    Add changes based on CR Feedback

--- emmet-game/src/components/EmmetListButton.js
+++ emmet-game/src/components/EmmetListButton.js
@@ -1,17 +1,11 @@
-import React from "react";
-
+import React from "react"
 
 export default function EmmetListButton(buttonName) {
-  //, linkedTo) {
-
-
-//<button onclick="window.location.href = '+ ... + ';">Hier klicken</button>
-
-  return (
-
-      <div className="button" >
-        <a href="https://docs.emmet.io/cheat-sheet/" target="_blank">{buttonName}</a>
-      </div>
-
-  )
+    return (
+        <div className="button">
+            <a href="https://docs.emmet.io/cheat-sheet/" target="_blank">
+                {buttonName}
+            </a>
+        </div>
+    )
 }
